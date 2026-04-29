08d95be0f3e7408a065e9532e312220942f99e58
Timothy Tiet <ttiet777@gmail.com>
2026-04-29 16:39:46 -0700

    test

---
+++ SMC.asm
@@ -0,0 +1,407 @@
+; TODO:
+;   PRINT when RESULT > 9, as it cannot print a multi-digit value (0-9 is the only numbers in the ascii table)
+
+; REGISTERS
+; R0: Primary
+; R1: (FREE) Current Value
+; R2: (FREE) Previous Value
+; R3: Operator Vector for JSRR
+; R4: RPN Stack Pointer
+; R5: RESULT Value
+; R6: (DON'T USE) (PURPOSE: STACK)
+; R7: (DON'T USE) (PURPOSE: RET address)
+
+                .ORIG x0026
+                .FILL INPUT         ; Prompt for user input (similar to IN)
+                .end
+                
+                .ORIG x0027
+                .FILL FLIP_SIGNS    ; Changes signs of R2 and R1 (For multiplication and division)
+                .end
+                
+                .ORIG x300
+_PROMPT_NEXT    .FILL x3E   ; '>'
+INPUT           LD R0, _PROMPT_NEXT
+                OUT
+                GETC
+                OUT
+                RTI
+                .end
+                
+                .ORIG x400
+FLIP_SIGNS      NOT R2, R2
+                ADD R2, R2, #1
+                NOT R1, R1
+                ADD R1, R1, #1
+                RTI
+                .end
+
+; ================================================================================
+; ==============              START OF INITALIZATION                ==============
+; ================================================================================
+
+                .ORIG x3000
+                BR START
+;   -= STRINGS =-
+JUST_LINES      .stringz "\n=============================================\n"
+NEW_LINE        .stringz "\n"
+ERROR_MESSAGE   .stringz "ERROR. RESTARTING PROGRAM..."
+PROMPT_START    .stringz "SMC RPN calculator\nEnter 0-9 or +, -, *, /, or . to display result on TOS"
+PRINT_TOS       .stringz "\nResult: "
+
+;   -= ERRORS =-
+_INPUT_ERROR    .FILL x3F   ; '?'
+_STACK_ERROR    .FILL x24   ; '$'
+_NUM_ERROR      .FILL x21   ; '!'
+_UNDEFINED_ERROR .stringz "undefined"
+
+;   -= VALID CHAR =-
+_ZERO           .FILL x30   ; '0'
+_NINE           .FILL x39   ; '9'
+_ADD            .FILL x2B   ; '+'
+_SUBTRACT       .FILL x2D   ; '-'
+_MULTIPLY       .FILL x2A   ; '*'
+_DIVIDE         .FILL x2F   ; '/'
+_PERIOD         .FILL x2E   ; "."
+_COMMA          .FILL x2C   ; ","
+_ENTER          .FILL x0A   ; ENTER KEY
+
+;   -= VARIABLES =-
+STACK           .FILL x4000
+SIZE            .FILL #0    ; SIZE OF STACK
+RESULT          .FILL #0
+
+;   -= OTHER =-
+DECIMAL_TO_CHAR .FILL #48
+CHAR_TO_DECIMAL .FILL #-48
+
+; ================================================================================
+; ===============              END OF INITALIZATION                ===============
+; ================================================================================
+; ===============                MAIN PROGRAM                       ==============
+; ================================================================================
+            
+START           LD  R4, STACK
+                LEA R0, PROMPT_START
+                PUTS
+                LEA R0, NEW_LINE
+                PUTS
+                
+INPUT_LOOP      TRAP x26            ;   INPUT
+                JSR OPERATOR_CHK    ;   CHECK FOR OPERATORS
+                JSR PERIOD_CHK      ;   CHECK FOR PERIOD "."
+                JSR INVALID_CHK     ;   CHECK FOR INVALID CHARACTERS
+                LD  R1, CHAR_TO_DECIMAL
+                ADD R0, R0, R1
+                JSR PUSH            ;   IS 0-9, ADD TO STACK
+                BR INPUT_LOOP
+                BR TOS_CHECK
+                
+; ================================================================================
+; ================                FUNCTIONS                       ================
+; ================================================================================
+
+INVALID_CHK     LD  R1, _ENTER      ;   R0 = ENTER_KEY
+                NOT R1, R1
+                ADD R1, R1, #1
+                ADD R1, R0, R1
+                BRz TOS_CHECK       ;   Compute result if true
+                LD  R1, _NINE       ;   R0 = '9'
+                NOT R1, R1
+                ADD R1, R1, #1
+                ADD R1, R0, R1
+                BRp ERROR_INVALID   ;   INVALID INPUT (R0 > x39 ('9'))
+                LD  R1, _ZERO       ;   R0 = '0'
+                NOT R1, R1
+                ADD R1, R1, #1
+                ADD R1, R0, R1
+                BRn ERROR_INVALID   ;   INVALID INPUT (R0 < x2A ('0'))
+                LD  R1, _COMMA      ;   R0 = ','
+                NOT R1, R1
+                ADD R1, R1, #1
+                ADD R1, R0, R1
+                BRz ERROR_INVALID   ;   INVALID INPUT (R0 == x2C (','))
+                RET
+                
+PERIOD_CHK      LD  R1, _PERIOD     ;   R0 = '.'
+                NOT R1, R1
+                ADD R1, R1, #1
+                ADD R1, R0, R1
+                BRz TOS_CHECK       ;   Check Top of Stack if R0 == '.'
+                RET
+                
+TOS_CHECK       LD  R2, SIZE
+                BRz ERROR_STACK     ;   EMPTY STACK -> ERROR (Can't check empty)
+                LDR R0, R4, #0      ;   TOS value
+                ADD R5, R0, #0
+                ST  R0, RESULT
+_TOS_PRINT      LEA R0, PRINT_TOS
+                PUTS
+                LD  R0, RESULT
+                LD  R1, DECIMAL_TO_CHAR
+                ADD R0, R0, R1
+                OUT
+                BR INPUT_LOOP
+
+; ================================================================================
+; =============                ERROR FUNCTIONS                       =============
+; ================================================================================
+
+ERROR           LEA R0, JUST_LINES
+                PUTS
+                LEA R0, ERROR_MESSAGE
+                PUTS
+                LEA R0, JUST_LINES
+                PUTS
+                AND R0, R0, #0      ; Resets SIZE
+                ST  R0, SIZE
+                BR  START
+
+ERROR_INVALID   LD  R0, _INPUT_ERROR
+                OUT
+                BR ERROR
+               
+ERROR_STACK     LD  R0, _STACK_ERROR
+                OUT
+                BR  ERROR
+
+ERROR_NUM       LD  R0, _NUM_ERROR
+                OUT
+                BR  ERROR
+                
+ERROR_UNDEFINED LEA R0, _UNDEFINED_ERROR    ; Divide by 0 case
+                PUTS
+                BR  ERROR
+
+; ================================================================================
+; =============                STACK FUNCTIONS                       =============
+; ================================================================================
+
+PUSH            ADD R4, R4, #-1     ;   Decrement
+                STR R0, R4, #0      ;   Store
+                LD  R2, SIZE        ;   SIZE++
+                ADD R2, R2, #1
+                ST  R2, SIZE
+                RET
+                
+POP             LDR R0, R4, #0      ;   Load
+                ADD R4, R4, #1      ;   Increment
+                LD  R2, SIZE        ;   SIZE--
+                ADD R2, R2, #-1
+                ST  R2, SIZE
+                RET
+
+; ================================================================================
+; ============                OPERATOR FUNCTIONS                       ===========
+; ================================================================================
+
+OPERATOR_CHK    LD  R1, _ADD
+                NOT R1, R1
+                ADD R1, R1, #1
+                ADD R1, R0, R1
+                BRz ADD_SUBROUTINE
+                LD  R1, _SUBTRACT
+                NOT R1, R1
+                ADD R1, R1, #1
+                ADD R1, R0, R1
+                BRz SUB_SUBROUTINE
+                LD  R1, _MULTIPLY
+                NOT R1, R1
+                ADD R1, R1, #1
+                ADD R1, R0, R1
+                BRz MUL_SUBROUTINE
+                LD  R1, _DIVIDE
+                NOT R1, R1
+                ADD R1, R1, #1
+                ADD R1, R0, R1
+                BRz DIV_SUBROUTINE
+                RET                 ;   NO OPERATOR WAS FOUND
+                
+ADD_SUBROUTINE  LEA R3, ADDITION
+                BR  EXECUTE_OPERATOR
+                
+SUB_SUBROUTINE  LEA R3, SUBTRACTION
+                BR  EXECUTE_OPERATOR
+                
+MUL_SUBROUTINE  LEA R3, MULTIPLICATION
+                BR  EXECUTE_OPERATOR
+                
+DIV_SUBROUTINE  LEA R3, DIVISION
+                BR  EXECUTE_OPERATOR
+
+EXECUTE_OPERATOR LD  R2, SIZE
+                ADD  R2, R2, #-1
+                BRnz ERROR_STACK    ;   SIZE >= 2, else invalid computation
+                JSR  POP
+                ADD  R1, R0, #0     ;   (...) +-*/ (THIS)
+                JSR  POP
+                ADD  R2, R0, #0     ;   (THIS) +-*/ (...)
+                AND  R0, R0, #0
+                JSRR R3             ;   JUMP TO SAVED OPERATOR FUNCITON
+IS_VALID        JSR  PUSH
+                BR   INPUT_LOOP
+
+; ========================================================
+; ============      ADDITION / SUBTRACTION    ============
+; ========================================================
+; R2+ & R1+ = (-)
+; R2- & R1- = (+)
+; Otherwise, do a regular add
+        
+SUBTRACTION     NOT R1, R1          ;   Flip signs of R1
+                ADD R1, R1, #1
+ADDITION        ADD R2, R2, #0      ;   Check R2 Sign
+                BRp R2_IS_POS       ;   (+) + (?)
+                BRn R2_IS_NEG       ;   (-) + (?)
+                BRz REGULAR_ADD     ;    0  + (?)
+
+R2_IS_POS       ADD  R1, R1, #0
+                BRp  POS_ADD        ;   (+) + (+)
+                BRnz REGULAR_ADD    ;   (+) + (-) OR (+) + 0
+
+R2_IS_NEG       ADD  R1, R1, #0
+                BRzp REGULAR_ADD    ;   (-) + (+) OR (-) + 0
+                BRn  NEG_ADD        ;   (-) + (-)
+                
+POS_ADD         ADD  R0, R2, R1
+                BRnz ERROR_NUM      ;   (+) + (+) = (-) means an OVERFLOW occured
+                RET
+
+NEG_ADD         ADD  R0, R2, R1
+                BRzp ERROR_NUM      ;   (-) + (-) = (+) means an UNDERFLOW occured
+                RET
+                
+; Its impossible for overflow/underflow here as R1 or R2 cannot hold a value more than 16-bits
+; As such, if (+) + (-) or vice versa, it'll always equal to something else less together
+REGULAR_ADD     ADD R0, R2, R1
+                RET
+
+; ================================================
+; ============      MULTIPLICATION    ============
+; ================================================
+
+MULTIPLICATION  ADD R1, R1, #0
+                BRz TIMES_ZERO      ;   R2 * 0
+                BRp SKIP            ;   R2 * (+)
+                TRAP x27            ;   R1 is negative, flip signs of both
+SKIP            ADD R2, R2, #0
+                BRn NEG_MULT
+                BRp POS_MULT
+                BRz TIMES_ZERO
+
+;   -= POSITIVE MULTIPLCATION =-
+POS_MULT        ADD  R0, R2, #0     ;   Multiply by 1
+                ADD  R1, R1, #-1
+                BRz  FINISHED_MULT
+POS_MULT_NEXT   ADD  R0, R0, R2     ;   Multiply (R0 + R2)
+                BRnz ERROR_NUM      ;   [ (+) * (-) = (+) ] -> OVERFLOW
+                ADD  R1, R1, #-1    ;   Decrement loop
+                BRp  POS_MULT_NEXT  ;   REPEAT
+                BR   FINISHED_MULT
+
+;   -= NEGATIVE MULTIPLCATION =-
+NEG_MULT        ADD R0, R2, #0      ;   Multiply by 1
+                ADD R1, R1, #-1
+                BRz FINISHED_MULT
+NEG_MULT_NEXT   ADD R0, R0, R2      ;   Multiply (R0 + R2)
+                BRzp ERROR_NUM      ;   [ (+) * (-) = (+) ] -> UNDERFLOW
+                ADD R1, R1, #-1     ;   Decrement loop
+                BRp NEG_MULT_NEXT   ;   REPEAT
+                BR  FINISHED_MULT
+
+;   -= CONDITIONS =-
+TIMES_ZERO      AND R0, R0, #0
+                RET
+                
+TIMES_ONE       ADD R0, R2, #0
+                RET
+                
+FINISHED_MULT   RET
+
+; ==========================================
+; ============      DIVISION    ============
+; ==========================================
+
+DIVISION        ADD R1, R1, #0
+                BRz ERROR_UNDEFINED ;    Cannot divide by zero
+                BRp DIV_CHK_R2      ;    R1 is already positive, move on
+                TRAP x27            ;    R1 is negative, so flip BOTH
+DIV_CHK_R2      ADD R2, R2, #0
+                BRp DIV_POS_SETUP   ;    R2 positive -> Positive division
+                BRn DIV_NEG_SETUP   ;    R2 negative -> Negative division
+                AND R0, R0, #0      ;    R2 = 0, so finished
+                RET
+
+;   -= NEGATIVE DIVISION =-           
+DIV_NEG_SETUP   TRAP x27            ;    R2 negative -> Setup for Negative Division
+DIV_NEG_LOOP    ADD R2, R2, R1      ;    R2 - R1
+                BRn NEG_DONE        ;    R2 < 0 = STOP
+                ADD R0, R0, #1      ;    Count 1 successful division
+                BR DIV_NEG_LOOP
+NEG_DONE        NOT R0, R0          ;    Flip result to negative
+                ADD R0, R0, #1
+                RET
+
+;   -= POSTIIVE DIVISION =-
+DIV_POS_SETUP   NOT R1, R1
+                ADD R1, R1, #1
+DIV_POS_LOOP    ADD R2, R2, R1      ;    R2 - R1
+                BRn POS_DONE        ;    R2 < 0 = STOP
+                ADD R0, R0, #1      ;    Count 1 successful division
+                BR DIV_POS_LOOP
+POS_DONE        RET
+
+END             HALT
+                .END
+                
+
+; ===============================================
+; ============      LOGIC PROCESS    ============
+; ===============================================
+; -= MULTIPLICATION =-
+; R0 = R2 * R1
+; Process (WITHOUT OVERFLOW/UNDERFLOW CHECK):
+;   if (R1 == 0) {
+;       return 0
+;   } else if (R1 > 0) {
+;       R0 = multiply()
+;   } else if (R1 < 0) {
+;       R1 = R1 * -1
+;       R2 = R2 * -1
+;       R0 = multiply()
+;   }
+;   return R0
+
+;   multiply() {
+;       R0 = R0 + R2
+;       R1--
+;       if (R1 == 0) { 
+;           return R0 
+;       }
+;       return multiply()
+;   }
+
+
+; -= DIVISION =-
+; R0 = R2 / R1
+; Process (WITHOUT OVERFLOW/UNDERFLOW CHECK):
+;   if (R1 == 0) {                  ; R2 / R1 = Undefined
+;     return ERROR
+;   } else if (R1 < 0) {            ; Flip signs for proper condition checks
+;       R1 = R1 * -1
+;       R2 = R2 * -1
+;   }
+;
+;   if (R2 > 0) {                   ; Positive Division
+;       R1 = R1 * -1
+;       R0 = divide() [DIV_POS_SETUP]
+;       return R0
+;   } else if (R2 < 0) {            ; Negative Division
+;       R2 = R2 * -1
+;       R1 = R1 * -1
+;       R0 = divide() [DIV_NEG_SETUP]
+;       R0 = R0 * -1
+;       return R0
+;   } else if (R2 == 0) {           ; R2 / R1 = 0
+;       return R2
+;   }
